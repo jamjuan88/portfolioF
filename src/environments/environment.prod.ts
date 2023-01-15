58ad1439bdc561c47444fb4582f42b6bd96f511c
jamjuan88 <ing.juan.martinez.88@gmail.com>
2023-01-14 23:39:40 -0300

    subiendo el frontFinal

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'frontfinaljam',
+    appId: '1:200170070149:web:27dcc68580276b78adc9ec',
+    storageBucket: 'frontfinaljam.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyAW0o_Cb84LrUC5DYbXxDZht6jWq7y8hXc',
+    authDomain: 'frontfinaljam.firebaseapp.com',
+    messagingSenderId: '200170070149',
+  },
+  production: true,
+  URL: 'http://localhost:8080/'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'frontfinaljam',
+    appId: '1:200170070149:web:27dcc68580276b78adc9ec',
+    storageBucket: 'frontfinaljam.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyAW0o_Cb84LrUC5DYbXxDZht6jWq7y8hXc',
+    authDomain: 'frontfinaljam.firebaseapp.com',
+    messagingSenderId: '200170070149',
+  },
+
+  production: false,
+  URL: 'http://localhost:8080/'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
